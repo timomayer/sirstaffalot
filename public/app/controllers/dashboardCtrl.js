0199f73744e3eed3a8d14c5a84c558f1eb16bc0f
timomayer <timo.mayer@virtual-identity.com>
2013-11-07 18:51:23 +0100

    angular work

---
+++ public/app/controllers/dashboardCtrl.js
@@ -0,0 +1,12 @@
+/*global todomvc, angular */
+'use strict';
+
+/**
+ * The main controller for the app. The controller:
+ * - retrieves and persists the model via the todoStorage service
+ * - exposes the model to the template and provides event handlers
+ */
+staffalotApp.controller('dashboardCtrl', function dashboardCtrl($scope, $location, filterFilter) {
+
+    $scope;
+});
