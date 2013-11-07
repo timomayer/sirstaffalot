46de47f15c13ce07ed7c0a677d90aaa1b0b9ec1a
Dave <david.losert@gmx.de>
2013-11-07 23:55:53 +0100

    Added some more validations

--- model/Assignable.js
+++ model/Assignable.js
@@ -4,7 +4,7 @@ module.exports = function(sequelize, DataTypes) {
  return sequelize.define('Assignable', {
     name:               { type: DataTypes.STRING, allowNull: false },
     type:               { type: DataTypes.STRING, allowNull: false, defaultValue: 'project' }, // ['project', 'public holiday', 'vacation', 'not available', 'sick', ...]
-    fromEmail:          { type: DataTypes.STRING },
+    fromEmail:          { type: DataTypes.STRING, validate: { isEmail: true}},
     fromBusinessUnit:   { type: DataTypes.STRING },
     days:               { type: DataTypes.FLOAT },
     startDate:          { type: DataTypes.DATE },
--- model/TeamMember.js
+++ model/TeamMember.js
@@ -3,7 +3,7 @@ module.exports = function(sequelize, DataTypes) {
     return sequelize.define('TeamMember', {
         name:               { type: DataTypes.STRING, allowNull: false },
         type:               { type: DataTypes.STRING, allowNull: false, defaultValue: 'internal' }, // ['internal', 'external']
-        email:              { type: DataTypes.STRING }
+        email:              { type: DataTypes.STRING, validate: { isEmail: true} }
     }, {
 
         freezeTableName: true
