c224608a1bed981efa691ed5a33a3f125b47b303
aweisser <armin.weisser@gmail.com>
2013-11-07 13:16:51 +0100

    added Logger and config from leadbox

--- utils/logger.js
+++ utils/logger.js
@@ -1,25 +1,33 @@
 /**
- * User: David
- * Date: 07.11.13
- * Time: 11:56
- * To change this template use File | Settings | File Templates.
+ * Logger utils
+ *
+ * Usage:
+ * var log = require('./utils/logger.js');
+ * log.log('info', "127.0.0.1 - there's no place like home");
+ * log.log('warn', "127.0.0.1 - there's no place like home");
+ * log.log('error', "127.0.0.1 - there's no place like home");
+ * log.info("127.0.0.1 - there's no place like home");
+ * log.warn("127.0.0.1 - there's no place like home");
+ * log.error("127.0.0.1 - there's no place like home");
  */
 
-
-
 var winston = require('winston');
-
-
-var logger = new (winston.Logger)({
+if (GLOBAL.config) {
+    var loglevel = GLOBAL.config.loglevel;
+}
+else {
+    loglevel = 'debug';
+}
+var log = new (winston.Logger)({
     transports: [
-        new (winston.transports.Console)({
-            level: 'debug',
-            colorize: true,
-            timestamp: true
-        })
-//        new (winston.transports.File)({ filename: 'somefile.log' })
-    ]
+        new (winston.transports.Console)({ level: loglevel, json: false, timestamp: true }),
+        //new winston.transports.File({ filename: __dirname + '/debug.log', json: false })
+    ],
+    exceptionHandlers: [
+        new (winston.transports.Console)({ json: false, timestamp: true }),
+        //new winston.transports.File({ filename: __dirname + '/exceptions.log', json: false })
+    ],
+    exitOnError: false
 });
 
-
-module.exports = logger;
+module.exports = log;
