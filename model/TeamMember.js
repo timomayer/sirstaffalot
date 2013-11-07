7a123aeb3559c8a2f02127c41b714bfa3508b7ad
Dave <david.losert@gmx.de>
2013-11-07 17:02:05 +0100

    Implemented new sequelizer version

--- model/TeamMember.js
+++ model/TeamMember.js
@@ -1,9 +1,11 @@
 // @todo Validation for fields?
 module.exports = function(sequelize, DataTypes) {
     return sequelize.define('TeamMember', {
-        id:                 { type: DataTypes.INTEGER, primaryKey: true, allowNull: false, autoIncrement:true },
         name:               { type: DataTypes.STRING, allowNull: false },
         type:               { type: DataTypes.STRING, allowNull: false, defaultValue: 'internal' }, // ['internal', 'external']
         email:              { type: DataTypes.STRING }
+    }, {
+
+        freezeTableName: true
     });
 }
