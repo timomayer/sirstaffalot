442a1d65d083383f2490adfef20ec1d5e65c1f89
Dave <david.losert@gmx.de>
2013-11-08 00:49:35 +0100

    Added new idea for ressource-object

---
+++ public/_mock/newRessourceMock.js
@@ -0,0 +1,20 @@
+allProject['PROJEKTNAME']['KW']['USERNAME'] = COUNT;
+
+allProjects = {
+    'fancy SUV' : {
+        'KW1' : {
+            'TIMO' : 1,
+            'ARMIN' : 2
+    },
+    'KW2' : {
+        'DAVID' : 3
+    }
+},
+    'Leadböxle' : {
+
+}
+
+
+}
+
+
