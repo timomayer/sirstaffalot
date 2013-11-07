f0c4e912b919b2ea66825bf7ca7c7697e577d90a
aweisser <armin.weisser@gmail.com>
2013-11-07 22:31:02 +0100

    added testdata handling

--- model/Assignment.js
+++ model/Assignment.js
@@ -5,7 +5,6 @@ module.exports = function(sequelize, DataTypes) {
         year:               { type: DataTypes.INTEGER, allowNull: false, primaryKey: true },
         days:               { type: DataTypes.FLOAT, allowNull: false}
     }, {
-
         freezeTableName: true
     });
 }
--- scripts/createTestdata.js
+++ scripts/createTestdata.js
@@ -1,20 +1,22 @@
 var models = require('../model/model.js');
 var teamMembersJson = require('./testdata/teamMembers.json');
 var assignablesJson = require('./testdata/assignables.json');
+var assignmentsJson = require('./testdata/assignments.json');
 var log = require('../utils/logger.js');
 
 var TeamMember = models['TeamMember'];
 var Assignable = models['Assignable'];
+var Assignment = models['Assignment'];
 
 log.info('Adding testdata')
-log.info('TeamMembers: '+teamMembersJson);
 
+log.info('TeamMembers: '+teamMembersJson);
 TeamMember
     .bulkCreate(teamMembersJson)
     .success(function() {
         TeamMember.findAll().success(function(teamMember) {
             log.info(teamMember.name);
-        })
+        });
     }).error(function(error) {
         log.error(error);
     });
@@ -25,7 +27,18 @@ Assignable
     .success(function() {
         Assignable.findAll().success(function(assignable) {
             log.info(assignable.name);
-        })
+        });
+    }).error(function(error) {
+        log.error(error);
+    });
+
+log.info('Assignments: '+assignmentsJson);
+Assignment
+    .bulkCreate(assignmentsJson)
+    .success(function() {
+        Assignment.findAll().success(function(assignment) {
+            log.info(assignment);
+        });
     }).error(function(error) {
         log.error(error);
     });
