bb480270ec3170a1f5dcdad57a95943af5d752e1
aweisser <armin.weisser@gmail.com>
2013-11-07 19:22:05 +0100

    added testdata

---
+++ scripts/createTestdata.js
@@ -0,0 +1,33 @@
+var models = require('../model/model.js');
+var teamMembersJson = require('./testdata/teamMembers.json');
+var assignablesJson = require('./testdata/assignables.json');
+var log = require('../utils/logger.js');
+
+var TeamMember = models['TeamMember'];
+var Assignable = models['Assignable'];
+
+log.info('Adding testdata')
+log.info('TeamMembers: '+teamMembersJson);
+
+TeamMember.
+
+TeamMember
+    .bulkCreate(teamMembersJson)
+    .success(function() {
+        TeamMember.findAll().success(function(teamMember) {
+            log.info(teamMember.name);
+        })
+    }).error(function(error) {
+        log.error(error);
+    });
+
+log.info('Assignable: '+assignablesJson);
+Assignable
+    .bulkCreate(assignablesJson)
+    .success(function() {
+        Assignable.findAll().success(function(assignable) {
+            log.info(assignable.name);
+        })
+    }).error(function(error) {
+        log.error(error);
+    });
