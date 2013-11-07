5325dbda6f335ce3ccac5756b50b7c1db495a2b3
Dave <david.losert@gmx.de>
2013-11-07 13:48:09 +0100

    Renamed mySQLConnector to db and implemented sequlizer

---
+++ model/db.js
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: David
+ * Date: 07.11.13
+ * Time: 11:50
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+
+var log = require('../utils/logger.js');
+var Sequelize = require("sequelize");
+
+           var db = {};
+
+
+exports.db = Sequelize;
