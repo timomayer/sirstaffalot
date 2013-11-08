24631d7086cc9db2b0d48342b276dae94ae9b873
aweisser <armin.weisser@gmail.com>
2013-11-08 01:37:52 +0100

    added testdata handling

--- services/AssignmentService.js
+++ services/AssignmentService.js
@@ -1,7 +1,17 @@
 var _ = require('underscore');
 var log = require('../utils/logger.js');
 var models = require('../model/model.js');
+var sequelize = require('../model/model.js').sequelize;
+var sprintf = require('sprintf').sprintf;
 
+var assignmentsSQL = "select assignable.name assignableName, assignable.id assignableId, assignment.cw, teammember.id teamMemberId, assignment.days, teammember.name teamMemberName, assignable.days assignableDays from assignment " +
+    "inner join assignable on assignable.id = assignment.AssignableId " +
+    "inner join teammember on teammember.id = assignment.TeamMemberId" +
+    "where " +
+    "   assignment.year >= % and assignment.cw >= %" +
+    "   and " +
+    "   assignment.year <= % and assignment.cw <= %" +
+    "   order by assignable.name, assignable.id, assignment.cw, teammember.name, teammember.id;";
 
 module.exports = {
     getAssignmentsForTeamMembers: function (fromCW, toCW, callback) {
@@ -10,6 +20,12 @@ module.exports = {
 
     getAssignemntsForAssignables: function (fromCW, toCW, callback) {
 
+        var sql = sprintf(assignmentsSQL, fromCW.year, fromCW.cw, toCW.year, toCW.cw);
+        log.info(sql);
+        /*sequelize.query(sql, null, {raw: true}).success(function(resultSet) {
+            // trans
+            console.log(resultSet);
+        });*/
     }
 
 };
