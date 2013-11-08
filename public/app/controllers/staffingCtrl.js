3751957258a780cf83c92c0cccd54fa933995731
Dave <david.losert@gmx.de>
2013-11-08 13:52:01 +0100

    First function try for cw to range

--- public/app/controllers/staffingCtrl.js
+++ public/app/controllers/staffingCtrl.js
@@ -1,4 +1,3 @@
-
 /*global todomvc, angular */
 'use strict';
 
@@ -13,28 +12,28 @@ staffalotApp.controller('staffingCtrl', function staffingCtrl($scope, $location,
 	$scope.cwRange.cwStart = 1;
 	$scope.cwRange.cwEnd = 2;
 
-	staffingStorage.getProjects().success(function(data, status, headers, config) {
+	staffingStorage.getProjects().success(function (data, status, headers, config) {
 		$scope.projectsData = mapResultsetToProjectAssignment(data);
 	});
 
-	$scope.$watch('cwRange', function() {
-		staffingStorage.getProjects($scope.cwRange.cwStart, $scope.cwRange.cwEnd).success(function(data, status, headers, config) {
+	$scope.$watch('cwRange', function () {
+		staffingStorage.getProjects($scope.cwRange.cwStart, $scope.cwRange.cwEnd).success(function (data, status, headers, config) {
 			$scope.projectsData = data;
 		});
 
-		resourcesStorage.getResources($scope.cwRange.cwStart, $scope.cwRange.cwEnd).success(function(data, status, headers, config) {
+		resourcesStorage.getResources($scope.cwRange.cwStart, $scope.cwRange.cwEnd).success(function (data, status, headers, config) {
 			$scope.resourcesData = data;
 		});
 	}, true);
 
-	$scope.changeDatepicker = function() {
+	$scope.changeDatepicker = function () {
 		$scope.cwRange.cwStart = $('.cwPick1').val();
 		$scope.cwRange.cwEnd = $('.cwPick2').val();
 	};
 
 	function mapResultsetToProjectAssignment(resultSet, cwRange) {
 		var resultJSON = {};
-		angular.forEach(resultSet, function(currentRow) {
+		angular.forEach(resultSet, function (currentRow) {
 			var cwCoord = currentRow.year + '_' + currentRow.cw;
 			if (!resultJSON[currentRow.assignableId]) {
 				resultJSON[currentRow.assignableId] = {};
@@ -58,9 +57,9 @@ staffalotApp.controller('staffingCtrl', function staffingCtrl($scope, $location,
 			resultJSON[currentRow.assignableId]['cwsSum'] = {};
 		});
 
-		angular.forEach(resultJSON, function(project){
-			angular.forEach(cwRange, function(cw){
-				if(!project['cws'][cw]){
+		angular.forEach(resultJSON, function (project) {
+			angular.forEach(cwRange, function (cw) {
+				if (!project['cws'][cw]) {
 					project['cws'][cw] = [];
 				}
 			});
@@ -69,12 +68,35 @@ staffalotApp.controller('staffingCtrl', function staffingCtrl($scope, $location,
 	}
 
 	function recalculateCwSumsPerProject(projectData) {
-		angular.forEach(resultJSON[currentRow.assignableId]['cws'], function(crt, crtkey) {
+		angular.forEach(resultJSON[currentRow.assignableId]['cws'], function (crt, crtkey) {
 			resultJSON[currentRow.assignableId]['cwsSum'][crtkey] = 0;
-			angular.forEach(crt, function(cr) {
+			angular.forEach(crt, function (cr) {
 				resultJSON[currentRow.assignableId]['cwsSum'][crtkey] = resultJSON[currentRow.assignableId]['cwsSum'][crtkey] + cr.days;
 			});
 		});
+	}
+
+	function turnFromAndToCwToRangeArray(fromCWString, toCWString) {
+		var fromCW = fromCWString.split('_')[0];
+		var fromYear = fromCWString.split('_')[1];
+		var toCW = toCWString.split('_')[0];
+		var toYear = toCWString.split('_')[0];
+
+		var resultArray = [];
+		while ((fromCW <= toCW && fromYear === toYear) || (fromYear < toYear)) {
+			resultArray.push(fromYear + '_' + fromCW);
+
+			if(fromCW === 52){
+				if(moment('31.12.' + fromYear, 'DD.MM.YYYY').isoWeeks === 53){
+					fromCW++;
+				}
+				else {
+					fromCW = 1;
+					fromYear++;
+				}
+			}
+		}
+		return resultArray;
 	}
 
 });
