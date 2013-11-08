ed76f14abcaff9d5092570e87a1ad1c75e1d13ec
Dave <david.losert@gmx.de>
2013-11-08 01:49:58 +0100

    Fixed query

--- services/AssignmentService.js
+++ services/AssignmentService.js
@@ -7,10 +7,10 @@ var sprintf = require('sprintf').sprintf;
 var assignmentsSQL = "select assignable.name assignableName, assignable.id assignableId, assignment.cw, teammember.id teamMemberId, assignment.days, teammember.name teamMemberName, assignable.days assignableDays from assignment " +
     "inner join assignable on assignable.id = assignment.AssignableId " +
     "inner join teammember on teammember.id = assignment.TeamMemberId" +
-    "where " +
-    "   assignment.year >= % and assignment.cw >= %" +
+    " where " +
+    "   assignment.year >= %s and assignment.cw >= %s" +
     "   and " +
-    "   assignment.year <= % and assignment.cw <= %" +
+    "   assignment.year <= %s and assignment.cw <= %s" +
     "   order by assignable.name, assignable.id, assignment.cw, teammember.name, teammember.id;";
 
 module.exports = {
@@ -22,10 +22,12 @@ module.exports = {
 
         var sql = sprintf(assignmentsSQL, fromCW.year, fromCW.cw, toCW.year, toCW.cw);
         log.info(sql);
-        /*sequelize.query(sql, null, {raw: true}).success(function(resultSet) {
+        sequelize.query(sql, null, {raw: true}).success(function(resultSet) {
             // trans
             console.log(resultSet);
-        });*/
+        }).error(function(err){
+                console.log(err);
+            });
     }
 
 };
@@ -42,7 +44,7 @@ function mapToJSON(resultSet) {
         var userName = currentRow[5];
         var project_days = currentRow[6];
 
-        if (!resultJSON[project_id]) {
+        if (!resultJSON[currentRow.assignableId]) {
             resultJSON[project_id] = {};
             resultJSON[project_id].name = project_name;
             resultJSON[project_id].project_days = project_days;
