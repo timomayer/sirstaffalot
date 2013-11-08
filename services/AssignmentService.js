a2af66cf4d2906d5de8171b543b31261770aca33
Dave <david.losert@gmx.de>
2013-11-08 09:42:24 +0100

    Extracted map method to temp file

--- services/AssignmentService.js
+++ services/AssignmentService.js
@@ -32,34 +32,3 @@ module.exports = {
 
 };
 
-
-function mapToJSON(resultSet) {
-	var resultJSON = {};
-	_.each(resultSet, function (currentRow) {
-		var project_name = currentRow[0];
-		var project_id = currentRow[1];
-		var cw = currentRow[2];
-		var userId = currentRow[3];
-		var days = currentRow[4];
-		var userName = currentRow[5];
-		var project_days = currentRow[6];
-
-		if (!resultJSON[currentRow.assignableId]) {
-			resultJSON[project_id] = {};
-			resultJSON[project_id].name = project_name;
-			resultJSON[project_id].project_days = project_days;
-		}
-		if (!resultJSON[project_id][cw]) {
-			resultJSON[project_id][cw] = {};
-		}
-		if (!resultJSON[project_id][cw][userId]) {
-			resultJSON[project_id][cw][userId] =
-			{
-				days: days,
-				name: userName
-			}
-		}
-
-	});
-	return resultJSON;
-}
